99f7a3b77f06347ae148f03cc95ffdcda315d57f
Raman <raman.ranjan@elektrobit.com>
2022-07-30 18:17:18 +0530

    some basic few codes from which place order button will function and clicking it will navigate to other screen(Activity).

--- app/src/main/java/com/example/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MSG = "com.shiwam.multiScreen.ORDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void placeOrder(View view){
+        /*
+          We will handle the click on the place order button here
+          Build an intent to open another activity
+          */
+        Intent intent = new Intent(this, OrderActivity.class);
+        EditText editText1 = findViewById(R.id.editTextTextPersonName);
+        EditText editText2 = findViewById(R.id.editTextTextPersonName2);
+        EditText editText3 = findViewById(R.id.editTextTextPersonName3);
+        String message = "Order for" + editText1.getText().toString() + "," + editText2.getText().toString() + "&"
+                + editText3.getText().toString() + "has been successfully placed";
+        intent.putExtra(MSG, message);
+        startActivity(intent);
     }
 }
